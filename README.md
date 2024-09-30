24d0fd758a499474b1e0be2c5b9bb240790ed8ae
octal-zhihao <1187456954@qq.com>
2024-09-30 12:37:44 +0800

    feat(heloer.py/doc): add nowcoder

--- README.md
+++ README.md
@@ -1,47 +1,95 @@
-# ALL-CP-PLATFORMS_API
+# octal's cp-platforms-api
 
+This project is based on the code from: [SysSn13/all-cp-platforms-API](https://github.com/SysSn13/all-cp-platforms-API)
 ### Request Format:
-```sh
-http://all-cp-platforms-api.herokuapp.com/api/{platform}/{username}
+
+```bash
+http://cp-api.octalzhihao.top/api/{platform}/{username}
 ```
 
 
 Replace {platform} with the name of platform and {username} with the username of the user.
 
+> The username of the Nowcoder is the student id on the personal homepage
+
 ---
 ### Supported Platforms:
-- CodeChef
-- Codeforces
-- Atcoder
-- Spoj
-- Leetcode
+- [Codeforces](https://codeforces.com/)
+- [Atcoder](https://atcoder.jp/)
+- [Nowcoder](https://ac.nowcoder.com/)
 ---
 ### Deployment:
-The request url may not work in future so you can deploy it on [Heroku](https://www.heroku.com/) by following these steps easily:
+The request url may not work in future, so I recommend that you can deploy it on [Vercel](https://vercel.com/) by following these steps easily:
 #### 1. Using Heroku web
-- Create a new app on Heroku
-- Fork this repository.
-- Link the forked repo to the new app in the deployment section on Heroku.
-#### 2. Using heroku-cli
-- Clone the repo
+1. Create a new app on Vercel
+2. Fork this repository.
+3. Link the forked repo to the new app in the deployment section on Vercel.
+#### 2. Using Vercel-cli
+
+
+
+1. **Clone the Repository:**
+
+    First, you need to clone the repository from GitHub:
+
+    ```bash
+    git clone git@github.com:octal-zhihao/all-cp-platforms-api.git
+    ```
+
+    Navigate into the project directory:
+
+    ```bash
+    cd all-cp-platforms-api
+    ```
+
+2. **Install Vercel CLI:**
+
+    If you haven’t installed the Vercel CLI yet, you can do it globally using npm:
+
+    ```bash
+    npm install -g vercel
+    ```
+
+3. **Login to Vercel:**
+
+    Log in to your Vercel account using the CLI:
+
+    ```bash
+    vercel login
+    ```
+
+    This command will open a browser window where you can sign in to your Vercel account.
+
+4. **Deploy the Project:**
+    Now, run the following command to initialize the deployment process:
+    ```bash
+    vercel
+    ```
+
+    The Vercel CLI will ask you a series of questions to configure your project:
+
+    - **Link to existing project?** (You can select "No" if this is the first deployment.)
+    - **What’s your project’s name?** (You can accept the default name or provide a new one.)
+    - **In which scope do you want to deploy it?** (Choose your Vercel account or team.)
+    - **Which directory is your code located in?** (Typically the current directory, so press `Enter`.)
+    - **Want to override the settings?** (Usually, you can skip this and choose "No".)
+
+    Once this is done, Vercel will automatically build and deploy your project.
+
+5. **Deploy to Production:**
+
+    After configuring the project, you can deploy it to production using:
+
+    ```bash
+    vercel --prod
+    ```
+
+    This command ensures that your project is deployed to the production environment.
+
+6. **Access Your API:**
+    Once the deployment is successful, Vercel will provide a unique URL for your API. You can start making requests using this URL in the format:
+
     ```sh
-    git clone https://github.com/sudesh1122/all-cp-platforms-API.git
+    https://your-project-name.vercel.app/api/{platform}/{username}
     ```
-    ```
-    cd all-cp-platforms-API
-    ```
-
-- login to your Heroku account
-    ```
-    heroku login
-    ```
-- create a new app
-    ```
-    heroku create
-    ```
-- build the app
-    ```
-    git push heroku master
-    ```
-
- [![License](http://img.shields.io/:license-mit-blue.svg?style=flat-square)](http://badges.mit-license.org)
+   
--- app/helper.py
+++ app/helper.py
@@ -10,72 +10,6 @@ class User:
     def __init__(self,username=None,platform=None):
         self.__username = username
         self.__platform = platform
-    def codechef(self):
-        url = "https://codechef.com/users/{}".format(self.__username)
-        session = HTMLSession()
-        r = session.get(url,timeout=10)
-        if r.status_code!=200:
-            raise UsernameError("User not found")
-        try:
-            rating_header = r.html.find(".rating-header",first=True)
-        except:
-            raise UsernameError('User not found')
-
-        try:
-            rating = rating_header.find(".rating-number",first=True).text
-        except:
-            raise UsernameError('User not found')
-        max_rating = rating_header.find('small')[0].text[1:-1].split()[2]
-        rating_star = len(r.html.find(".rating-star",first=True).find('span'))
-        ranks = r.html.find('.rating-ranks',first=True).find('strong')
-        global_rank = ranks[0].text
-        country_rank = ranks[1].text
-        def get_contests_details():
-            rating_table = r.html.find('.rating-table',first=True)
-            data_rows = rating_table.find('tr')[1:]
-            data = list()
-            for row in data_rows:
-                td = row.find('td')
-                d = dict()
-                d['name'] = td[0].text.replace("\n", " ")
-                d['rating'] = td[1].text
-                d['global_rank'] = td[2].text
-                d['country_rank'] = td[3].text
-                data.append(d)
-            return data
-        def get_problems_solved():
-            div = r.html.find('.problems-solved',first=True)
-            problems_solved = dict()
-            fully_solved=dict()
-            partial_solved =dict()
-            articles = div.find('article')
-            h5s = div.find('h5')
-            for a,h in zip(articles,h5s):
-                ps = a.find('p')
-                prob_data = dict()
-                for p in ps:
-                    txt = p.text.split()
-                    type = txt[0][:-1]
-                    txt = txt[1:]
-                    prob_data[type] =list()
-                    for t in txt:
-                        if t==txt[len(txt)-1]:
-                            prob_data[type].append(t)
-                        else:
-                            prob_data[type].append(t[:-1])
-
-                if h.text.split()[0]=='Fully':
-                    fully_solved['count'] = h.text.split()[2][1:-1]
-                    fully_solved['problems'] = prob_data
-                elif h.text.split()[0]=='Partially':
-                    partial_solved['count'] = h.text.split()[2][1:-1]
-                    partial_solved['problems'] = prob_data
-            problems_solved['fully_solved'] = fully_solved
-            problems_solved['partial_solved'] = partial_solved
-            return problems_solved
-        return {'status':'OK','rating':rating,'max_rating':max_rating,
-                'global_rank':global_rank,'country_rank':country_rank,
-                'contests':get_contests_details(),'problems_solved':get_problems_solved()}
     def codeforces(self):
         url = 'https://codeforces.com/api/user.info?handles={}'.format(self.__username)
         r = requests.get(url,timeout=10)
@@ -109,90 +43,36 @@ class User:
                     val = val.split()[0]
                     data[attr]=val
         return data
-    def spoj(self):
-        url = "https://www.spoj.com/users/{}/".format(self.__username)
+    def nowcoder(self):
         session = HTMLSession()
-        r = session.get(url,timeout=10)
-        if r.status_code !=200:
-            raise UsernameError("User not found")
-        user_profile_left = r.html.find("#user-profile-left")
-        if not len(user_profile_left):
-            raise UsernameError
-        user_profile_left = user_profile_left[0]
+        url = "https://ac.nowcoder.com/acm/contest/profile/{}/".format(self.__username)
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+        r = session.get(url, headers=headers, timeout=10)
+        
+        if r.status_code != 200:
+            raise UsernameError('User not found')
+        check = r.html.find('.coder-name', first=True)
+        if check is None:
+            raise UsernameError('User not found')
         data = dict()
         data['status'] = 'OK'
-        data['full_name'] = user_profile_left.find('h3',first=True).text
-        data['img_src'] =   user_profile_left.find('img')[0].attrs['src']
-        p_data = user_profile_left.find('p')
-        data['location'] = p_data[0].text
-        data['joined'] = p_data[1].text.replace("Joined ","")
-        data['world_rank'] = p_data[2].text.split()[2][1:]
-        data['institution'] =  p_data[3].text.replace("Institution: ","")
-        data_stats = r.html.find('.profile-info-data-stats',first=True)
-        dts = data_stats.find('dt')
-        dds = data_stats.find('dd')
-        for dt,dd in zip(dts,dds):
-            data[dt.text] =dd.text
-        return data
-    def leetcode(self):
-        session = HTMLSession()
-        url = "https://leetcode.cn/u/{}/".format(self.__username)
-        r = session.get(url,timeout=10)
-        if r.status_code!=200:
-            raise UsernameError('User not found')
-        check = r.html.find('.username')
-        if not len(check):
-            raise UsernameError('User not found')
-        target = r.html.find('.list-group-item')
-        basic_profile = dict()
-        contest = dict()
-        progress = dict()
-        contribution = dict()
+        data['username'] = check.attrs.get('data-title')
+        target = r.html.find('.status-item')
         for li in target:
             text = li.text.split()
-            if len(text)<6:
-                    if len(text)>=2 and text[0]=='Location':
-                        basic_profile['location'] = li.text.replace("Location ","")
-                    elif len(text)>=1 and text[0]=='School':
-                        basic_profile['school'] = li.text.replace("School ","")
-                    elif len(text)>=2 and text[1]=='Rating':
-                        contest['rating']=text[0]
-                    elif len(text)>=3 and text[1]+text[2]=='FinishedContests':
-                        contest['finished_contests']=text[0]
-                    elif len(text)>=5 and text[len(text)-2]+text[len(text)-1]=='GlobalRanking':
-                        contest['global_ranking'] = text[0]
-                        contest['total_participants'] = text[2]
-                    elif len(text)>=5 and text[len(text)-2]+text[len(text)-1]=='SolvedQuestion':
-                        progress['solved_question'] = text[0]
-                        progress['total_question'] = text[2]
-                    elif len(text)>=5 and text[len(text)-2]+text[len(text)-1]=='AcceptedSubmission':
-                        progress['accepted_submission'] = text[0]
-                        progress['total_submission'] = text[2]
-                    elif len(text)>=4 and text[len(text)-2]+text[len(text)-1]=='AcceptanceRate':
-                        progress['acceptance_rate'] = text[0]+ " %"
-                    elif len(text)>=2 and text[1]=="Problems":
-                        contribution['problems'] = text[0]
-                    elif len(text)>=2 and text[1]=="Points":
-                        contribution['points']=text[0]
-                    elif len(text)>=3 and text[len(text)-2]+text[len(text)-1]=='TestCases':
-                        contribution['test_cases'] = text[0]
-                    elif len(text)>=2 and text[0] == 'Website':
-                        basic_profile['website'] = text[1]
-                    elif len(text)>=2 and text[0]=='Company':
-                        basic_profile['company'] = text[1]
-        data = {'status':'OK','basic_profile':basic_profile,'contest':contest,'progress':progress,'contribution':contribution,}
+            # print(text)
+            if len(text)>=2 and text[1]=='Rating':
+                data['rating']=text[0]
+            if len(text)>=2 and text[1]=='Rating排名':
+                data['ranking']=text[0]
         return data
     def get_info(self):
-        if self.__platform=='codechef':
-            return self.codechef()
         if self.__platform=='codeforces':
             return self.codeforces()
         if self.__platform == 'atcoder':
             return self.atcoder()
-        if self.__platform == 'spoj':
-            return self.spoj()
-        if self.__platform =='leetcode':
-            return self.leetcode()
+        if self.__platform =='nowcoder':
+            return self.nowcoder()
         raise PlatformError('Platform not Found')
 if __name__ == '__main__':
     platform = input("Enter platform: ")
